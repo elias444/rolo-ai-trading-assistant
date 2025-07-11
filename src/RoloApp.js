9117899bd02f7a390567b7a4fe6b4fc7f10dc505
elias444 <elias@elogroup.net>
2025-07-11 16:24:44 -0700

    Update RoloApp.js

--- src/RoloApp.js
+++ src/RoloApp.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function RoloApp() {
-  return (
-    <div>
-      <h1>Rolo AI Trading Assistant</h1>
-    </div>
-  );
+  return <h1>Rolo AI Trading Assistant</h1>;
 }
 
 export default RoloApp;
