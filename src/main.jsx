749837c1c209390f19065252f638082ebdb13c9c
elias444 <elias@elogroup.net>
2025-07-12 11:17:50 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RoloApp from './RoloApp.jsx';
-import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RoloApp />
-  </React.StrictMode>
+  <RoloApp />
 );
