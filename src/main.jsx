d28b46f78b8a5f1f30ef2ca51b2eb0e0e93f91a4
elias444 <elias@elogroup.net>
2025-07-16 17:41:01 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './RoloApp.jsx'; // Path to your main App component
+import RoloApp from './RoloApp.jsx';
 
-// Render the React application into the DOM
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RoloApp />
 );
