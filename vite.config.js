cfd9e3a3c37dc6ce2196c36fc98cc6191b239365
elias444 <elias@elogroup.net>
2025-07-20 12:43:23 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,31 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+    }),
+  ],
+  base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
-  }
+  },
+  server: {
+    proxy: {
+      '/.netlify/functions': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/.netlify\/functions/, ''),
+      },
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 });
